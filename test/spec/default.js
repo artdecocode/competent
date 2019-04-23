1509e6dccd8252058362fa162afff1197d04008c
Anton <anton@adc.sh>
2019-04-23 20:18:45 +0300

    add tests + fix new lines in attributes

--- test/spec/default.js
+++ test/spec/default.js
@@ -18,6 +18,27 @@ const T = {
     const m = mismatch(makeRe(keys), html, ['ws', 'all', 'key'])
     return m
   },
+  'makes a component re'() {
+    const re = makeRe(['Footer'])
+    const res = mismatch(re, '<Footer client="/test"/>', ['ws', 'test'])
+    equal(res.length, 1)
+    const res2 = mismatch(re, '<Footer/>', ['ws', 'test'])
+    equal(res2.length, 1)
+  },
+  'makes a component re with multiple lines'() {
+    const re = makeRe(['Footer'])
+    const res = mismatch(re, `<Footer client="/test"
+    clientLogo="test2"/>`, ['ws', 'test'])
+    equal(res.length, 1)
+  },
+  'makes two components of the same kind'() {
+    const re = makeRe(['Footer'])
+    const res = mismatch(re, `<Footer client="/test"
+    clientLogo="test2"/>
+    <Footer client="/test2"
+    clientLogo="test3"/>`, ['ws', 'test'])
+    equal(res.length, 2)
+  },
 }
 
 export default T

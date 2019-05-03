8c150be3b87ac0353df50461453eed194c392672
Anton <anton@adc.sh>
2019-05-03 23:12:42 +0300

    doc + change ? to =

--- externs.js
+++ externs.js
@@ -2,14 +2,17 @@
 /** @const */
 var _competent = {}
 /**
+ * The properties extracted from HTML and to be passed to the component for rendering.
  * @typedef {Object<string, *>}
  */
 _competent.Props
 /**
- * @typedef {{ export: function(boolean?), setPretty: function(boolean, number?) }}
+ * Service methods for `competent`.
+ * @typedef {{ export: function(boolean=), setPretty: function(boolean, number=) }}
  */
 _competent.Meta
 /**
- * @typedef {{ getId: (function(): string|undefined), getProps: (function(!_competent.Props, _competent.Meta)|undefined), markExported: (function(string, string, !_competent.Props, !Array<string>)|undefined), removeOnError: (boolean|undefined), onSuccess: (function(string)|undefined), onFail: (function(string, Error, number, string)|undefined) }}
+ * Options for the program. All functions will be called with the Replaceable instance as their `this` context.
+ * @typedef {{ getId: ((function(): string)|undefined), getProps: ((function(!_competent.Props, !_competent.Meta))|undefined), markExported: ((function(string, string, !_competent.Props, !Array<string>))|undefined), removeOnError: (boolean|undefined), onSuccess: ((function(string))|undefined), onFail: ((function(string, !Error, number, string))|undefined) }}
  */
 _competent.Config
